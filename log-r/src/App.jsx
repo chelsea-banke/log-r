a67b5ec989ab04c5ac015a4a6936f1de567cd632
chelsea-banke <chelseanewhorizon@gmail.com>
2023-02-07 19:01:39 +0100

    project initialisation and structuring

---
+++ log-r/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="">Hello World</div>
+  )
+}
+
+export default App
