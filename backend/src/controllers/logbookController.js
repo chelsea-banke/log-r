1234c490c42b0ea46fec165b36568e44cba76466
chelsea-banke <chelseanewhorizon@gmail.com>
2023-08-06 21:16:36 +0100

    logbook get api setup complete

--- backend/src/controllers/logbookController.js
+++ backend/src/controllers/logbookController.js
@@ -48,4 +48,43 @@ const createLogbook = async (req, res)=>{
     }
 }
 
-module.exports = { createLogbook }
+const getLogbook = (req, res)=>{
+    const email = res.locals.authEmail
+    const title = req.params["title"].slice(1)
+    console.log(title)
+    if(title){
+        Logbooks.findAll({"where": {
+            "user_id": email,
+            "title": title
+        }}).then(logbooks=>{
+            const logbook = logbooks[0]
+            if(logbook){
+                res.status(200).json({
+                    "success": true,
+                    "message": "logbook fetch successfull",
+                    "logbook": logbook
+                })
+            }
+            else{
+                res.status(400).json({
+                    "success": false,
+                    "message": `user ${email} has no logbook titled ${title}`,
+                })
+            }
+        }).catch(error=>{
+            res.status(401).json({
+                "success": false,
+                "message": "error fetching logbook",
+                "error": error.message
+            })
+        })
+    }
+    else{
+        res.status(400).json({
+            "success": false,
+            "message": `${title} title parameter`
+        })
+    }
+}
+
+module.exports = { createLogbook, getLogbook }
--- backend/src/routes/logbook.js
+++ backend/src/routes/logbook.js
@@ -1,8 +1,9 @@
-const { createLogbook } = require("../controllers/logbookController")
+const { createLogbook, getLogbook } = require("../controllers/logbookController")
 const userAuth = require("../middlewares/auth")
 
 const router = require("express").Router()
 
 router.route("/create-new").post(userAuth, createLogbook)
+router.route("/:title").get(userAuth, getLogbook)
 
 module.exports = router
