a1a9e19b9746eb86ded7d3c6f3bf92bbc687f191
chelsea-banke <chelseanewhorizon@gmail.com>
2023-08-06 21:57:51 +0100

    api for deleting logbook setup complete

--- backend/src/controllers/logbookController.js
+++ backend/src/controllers/logbookController.js
@@ -5,13 +5,11 @@ const fieldCheck = require("../utils/fieldCheck")
 const createLogbook = async (req, res)=>{
     const email = res.locals.authEmail
     const logbookData = req.body
-
     if(fieldCheck(logbookData)){
         await Logbooks.findAll({"where":{
             "title": logbookData["title"],
             "user_id": email
         }
-
         }).then(async logbooks=>{
             const logbook = logbooks[0]
             if(logbook){
@@ -23,7 +21,6 @@ const createLogbook = async (req, res)=>{
             else{
                 logbookData["user_id"] = email
                 await Logbooks.create(logbookData).then(results=>{
-                    console.log(results)
                     res.status(200).json({
                         "success": true,
                         "message": "successfull fetch of logbook",
@@ -51,7 +48,6 @@ const createLogbook = async (req, res)=>{
 const getLogbook = async (req, res)=>{
     const email = res.locals.authEmail
     const title = req.params["title"].slice(1)
-    console.log(title)
     if(title){
         await Logbooks.findAll({"where": {
             "user_id": email,
@@ -96,7 +92,6 @@ const updateLogbook = async (req, res)=>{
             "user_id": email,
             "title": title
         }}).then(async logbooks=>{
-            console.log(logbooks[0])
             if(logbooks[0]){
                 await Logbooks.update(logbookData, {"where": {
                     "user_id": email,
@@ -131,4 +126,46 @@ const updateLogbook = async (req, res)=>{
     }
 }
 
-module.exports = { createLogbook, getLogbook, updateLogbook }
+const deleteLogbook = async (req, res)=>{
+    const email = res.locals.authEmail
+    const title = req.params["title"].slice(1)
+    if(title){
+        await Logbooks.findAll({"where": {
+            "user_id": email,
+            "title": title
+        }}).then(async logbooks=>{
+            if(logbooks[0]){
+                await Logbooks.destroy({"where": {
+                    "user_id": email,
+                    "title": title
+                }}).then(results=>{
+                    res.status(200).json({
+                        "success": true,
+                        "message": `${title} successfully deleted`
+                    })
+                }).catch(error=>{
+                    res.status(401).json({
+                        "success": false,
+                        "message": "error deleting logbook",
+                        "error": error.message
+                    })
+                })
+            }
+            else{
+                res.status(400).json({
+                    "success": false,
+                    "message": `user ${email} has no logbook titled ${title}`,
+                })
+            }
+        })
+    }
+    else{
+        res.status(400).json({
+            "success": false,
+            "message": `${title} title parameter`
+        })
+    }
+}
+
+
+module.exports = { createLogbook, getLogbook, updateLogbook, deleteLogbook }
--- backend/src/routes/logbook.js
+++ backend/src/routes/logbook.js
@@ -1,4 +1,4 @@
-const { createLogbook, getLogbook, updateLogbook } = require("../controllers/logbookController")
+const { createLogbook, getLogbook, updateLogbook, deleteLogbook } = require("../controllers/logbookController")
 const userAuth = require("../middlewares/auth")
 
 const router = require("express").Router()
@@ -6,5 +6,6 @@ const router = require("express").Router()
 router.route("/create-new").post(userAuth, createLogbook)
 router.route("/:title").get(userAuth, getLogbook)
 router.route("/:title").put(userAuth, updateLogbook)
+router.route("/:title").delete(userAuth, deleteLogbook)
 
 module.exports = router
